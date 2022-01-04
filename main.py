4554ef5614a6d475e653857dd4934fcf8f9a4848
Arjun Thangaraju <axt7568@g.rit.edu>
2022-01-04 18:25:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Arjun Thangaraju'
+# ---------------------------------------------------------------------------
 # import libraries
 import os
 import pandas as pd
